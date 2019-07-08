0d8cacf0662fb995c63b2820274bf3d4ed93c2ca
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-08 15:18:51 -0400

    Reutilizando Variables de Otros Archivos y del Sistema

---
+++ reutilizandoVariables.bat
@@ -0,0 +1,12 @@
+REM Limpliamos Pantalla
+CLS
+
+REM Reutilizando Variables Que Ya Fueron Definidas en Otro Archivo
+ECHO Nombre Completo %nombre_completo%
+
+REM Variable que Define el Propio Sistema
+ECHO %OS%
+
+REM Variable Que define el sistema para el home del Usuario
+ECHO %HOMEPATH%
+
