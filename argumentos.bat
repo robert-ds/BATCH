15dbeb107af02cae48868617496c8d37fae7148b
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-09 17:32:15 -0400

    Paso de Argumentos a los Archivos

---
+++ argumentos.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+@REM
+
+ECHO Nombre del Programa %0
+ECHO Parametro del Programa %1
+
+ECHO.
+REM Instruccion que lista el directorio que le pasemos como argumento
+DIR %1
+
+REM Detenemos la Pantalla
+PAUSE
+
+REM LIMPIAMOS PANTALLA
+CLS
+
+
+
