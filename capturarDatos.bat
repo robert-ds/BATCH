c770927d59c1bac75105dcc64780f450c410fa46
Robert Vasquez <vasquez.robert.andres@gmail.com>
2019-07-09 17:51:17 -0400

    Formas de Capturar Datos que se Utilizan en el Flujo del Programa

---
+++ capturarDatos.bat
@@ -0,0 +1,23 @@
+@REM
+@ECHO OFF
+
+ECHO Digite su Nombre 
+SET /P nombre=
+
+ECHO Digite su Edad
+SET /P edad=
+
+ECHO.
+ECHO Su Nombre Es %nombre%
+
+ECHO.
+ECHO Su Edad Es %edad%
+ECHO.
+
+REM DESPLEGAMOS EL MENSAJE EN LA LINEA DE DECLARACION SET
+SET /P peso=Digite su Peso 
+ECHO Su Peso es %peso% KG
+ECHO.
+
+PAUSE
+CLS
